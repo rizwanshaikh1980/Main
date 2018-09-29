f32756a2ecda82898baac4fcb9c84aacdc593c32
Rizwan Shaikh <Rizwan@gmail.com>
2018-09-29 14:59:28 +0530

    maven project

---
+++ src/main/java/com/codebind/App.java
@@ -0,0 +1,13 @@
+package com.codebind;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
